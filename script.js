df8c960d762f249867b2dcac259090795f09634c
RYLIX191 <anyanwuann08@gmail.com>
2024-11-24 11:32:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web developer","Graphic designer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true,
+})
+/*==========aside=======*/
+const nav = document.querySelector('nav').scroll
+
